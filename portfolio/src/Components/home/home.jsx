b4abf3c7f524fce311ff5f75fa377d3fb2b9b68b
Lucas Melo <lmelo072002@gmail.com>
2022-10-27 18:46:30 -0300

    homepage sessão

--- portfolio/src/Components/home/home.jsx
+++ portfolio/src/Components/home/home.jsx
@@ -17,6 +17,25 @@ const Home = () => {
                         <li>portfolio</li>
                     </ul>
                 </header>
+                <section className='apresentation'>
+                    <div className='apresentation-redes'>
+                        <ul className='list-redes'>
+                            <li><FaGithubSquare className='icons' /> </li>
+                            <li><IoLogoLinkedin className='icons'/></li>
+                            <li><FaWhatsappSquare className='icons'/> </li>
+                            <li><MdEmail className='icons'/></li>
+                        </ul>
+                    </div>
+                    <div className='apresentation-tittle'>
+                        <div className='image-box'>
+                            <img src={Eu} alt="" className='image'/>
+                        </div>
+                        <div>
+                            <h1 className='tittle'>WEB DEVELOPER</h1> 
+                            <h2 className='subtittle'>Specialized in frontend</h2>   
+                        </div>
+                    </div>               
+                </section>
                 
             </div>
         </>
