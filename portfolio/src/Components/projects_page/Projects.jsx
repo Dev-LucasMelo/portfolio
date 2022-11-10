e3cac2f4c7ce1742be6429f296b0ff09cf1a87a2
Lucas Melo <lmelo072002@gmail.com>
2022-11-10 11:43:37 -0300

    Comp "sectionprojects" dinamico / Remoção de eventos DOM

--- portfolio/src/Components/projects_page/Projects.jsx
+++ portfolio/src/Components/projects_page/Projects.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Home from '../home/home';
 import Menu from '../menu/menu';
 import Sectionprojects from '../sectionprojetos/sectionprojects';
 
 import "./Projects.css"
 
+import Netflix from "./../../images/netflix.png"
+import eu from "./../../images/eu.png"
 
 const ops = [
     {
@@ -15,14 +16,27 @@ const ops = [
 ]
 
 const arrayprojects = [
-    
+    {
+        "id" : 1,
+        "background" : Netflix,
+        "tittle": "Netflix Copy Front-end",
+        "stacks" : "React|Axios",
+        "href" : "https://dev-lucasmelo.github.io/netflix_build/"
+    },
+    {
+        "id" : 2,
+        "background" : eu,
+        "tittle": "teste",
+        "stacks" : "React|Node",
+        "href" : "https://dev-lucasmelo.github.io/netflix_build/"
+    },
 ]
 
 const Projectspage = () => {
     return (  
         <>
             <Menu ops={ops} />
-            <Sectionprojects />
+            <Sectionprojects img={Netflix} data={arrayprojects} />
             
         </>
     );
--- portfolio/src/Components/sectionprojetos/sectionprojects.jsx
+++ portfolio/src/Components/sectionprojetos/sectionprojects.jsx
@@ -1,46 +1,55 @@
-import React from 'react';
+import React from "react";
+//import {useState} from "react"
+import "./sectionprojetos.css";
 
-import "./sectionprojetos.css"
+const Sectionprojects = (props) => {
 
-const Sectionprojects = () => {
-
-    function mousemove_event() {
-        const boxdata = document.getElementById("projectbox-data")
-        boxdata.classList.remove("some")
+    /* 
+    function mousemove_event(e) {  
+        const boxdata = document.getElementById(`projectbox-data1`);
+        boxdata.classList.remove("some");
     }
-
-    function mouseleave_event() {
-        const boxdata = document.getElementById("projectbox-data")
-        boxdata.classList.add("some")
+    
+      function mouseleave_event(e) {
+        const boxdata = document.getElementById("projectbox-data2");
+        boxdata.classList.add("some"); 
     }
+    */
 
-    return (
-        <section className='sectionprojects_comp'>
-            <h1>Projetos</h1>
-            <div className='projectboxall'>
-                <div
-                    onMouseLeave={mouseleave_event}
-                    onMouseMove={mousemove_event}
-                    className='project'>
-                    <div className='projectbox-data some' id='projectbox-data'>
-                        <div className='tittleprojects' >
-                            <h1>Netflix Copy Front-end</h1>
-                            <h4>React.js|Axios</h4>
-                        </div>
-                        <div>
-                           <a className='projectlink' href="" target="_blank"><p>clique para ver o projeto</p></a> 
-                        </div>
+  return (
+
+    <section className="sectionprojects_comp">
+      <h1>Projetos</h1>
+      <div className="projectboxall">
+
+        {
+            props.data.map((proj) => {
+                return (
+                    <div
+                    key={proj.id}
+                    className="project"
+                    style={{
+                          backgroundImage: `url(${proj.background})`
+                    }}
+                  >
+                    <div className="projectbox-data " id={`projectbox-data`}>
+                      <div className="tittleprojects">
+                        <h1>{proj.tittle}</h1>
+                        <h4>{proj.stacks}</h4>
+                      </div>
+                      <div>
+                        <a className="projectlink" href={proj.href} target="_blank">
+                          <p>clique para ver o projeto</p>
+                        </a>
+                      </div>
                     </div>
-                </div>
-                <div className='project'>
-
-                </div>
-                <div className='project'>
-
-                </div>
-            </div>
-        </section>
-    );
-}
+                  </div>
+                )
+            })   
+        }
+      </div>
+    </section>
+  );
+};
 
 export default Sectionprojects;
