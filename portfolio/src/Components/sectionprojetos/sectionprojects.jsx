b418e7157d9d79d3ed22aa1d8435db23eb4d07b5
Lucas Melo <lmelo072002@gmail.com>
2022-11-15 00:53:16 -0300

    Finalizando Eventos Projects

--- portfolio/src/Components/sectionprojetos/sectionprojects.jsx
+++ portfolio/src/Components/sectionprojetos/sectionprojects.jsx
@@ -1,58 +1,52 @@
 import React from "react";
-import {useEffect} from "react"
+import { useState } from "react"
 import "./sectionprojetos.css";
 
 const Sectionprojects = (props) => {
-
+  
   return (
     <section className="sectionprojects_comp">
       <h1>Projetos</h1>
       <div className="projectboxall">
+
         {props.data.map((proj) => {
-          //FUNCS IMGS
-        
-          const boxes = document.querySelectorAll(".project")
 
+          function Mouseevent(a) {
+            const itemalvo = a.target
+            itemalvo.style.background = "white"
+            itemalvo.style.transition = ".3s"
+            const filho = itemalvo.children
 
-          boxes.forEach(element => {
-            const backgrounddata = props.data[element.id].background
-             
-            element.addEventListener("mousemove",()=>{
-              element.style.backgroundImage = ""
-            })
-
-            element.addEventListener("mouseleave",()=>{
-              element.style.backgroundImage = `url(${backgrounddata})`   
-            })  
-
-            element.style.backgroundImage = `url(${backgrounddata})` 
-          });
-
-         //funcs texts
-     
-          function mousemove_event(e) {
-            const boxdata = e.target;
-            boxdata.children[0].style.display = "";
-            boxdata.children[1].style.display = "";
-
+            filho[0].style.display = "flex"
+            filho[1].style.display = "flex"
+          
           }
+          function Removemouseevent(a) {
+            const itemalvo = a.target
+            itemalvo.style.background = "black"
+            itemalvo.style.backgroundImage = `url(${proj.background})`
+            itemalvo.style.backgroundSize = "cover"
+            itemalvo.style.transition = ".3s"
 
-          function mouseleave_event(e) {
-            const boxdata = e.target;
-            boxdata.children[0].style.display = "none";
-            boxdata.children[1].style.display = "none";
+            const filho = itemalvo.children
 
+            filho[0].style.display = "none"
+            filho[1].style.display = "none"
+            
           }
 
           return (
             <div
+              onMouseMove={Mouseevent}
+              onMouseLeave={Removemouseevent}
               id={proj.id}
               key={proj.id}
               className="project"
+              style={{ backgroundImage: `url(${proj.background})` }}
             >
               <div
                 key={proj.id}
-                className="projectbox-data "
+                className="projectbox-data"
                 id={`projectbox-data`}
               >
                 <div className="tittleprojects">
